7694424a4a7011caeb87e2bb5c43abb1ef2403cc
guangtuan <zhuangguangtuan@gmail.com>
2022-11-15 20:28:57 +0800

    feat: show creation in any case

--- src/App.tsx
+++ src/App.tsx
@@ -13,34 +13,31 @@ const chunk = (arr: Array<SpaceVo>, size: number): Array<Array<SpaceVo>> => {
 
 const filterMapWhenResponseOk = (res: Response) => {
   if (res.ok) {
-    return res.json()
+    return res.json();
   } else {
-    return Promise.reject(new Error("接口响应失败"))
+    return Promise.reject(new Error("接口响应失败"));
   }
-}
+};
 
-const noop = (): void => {}
-const constTrue = () => true
-const constFalse = () => false 
+const noop = (): void => {};
+const constTrue = () => true;
+const constFalse = () => false;
 
 const useRunOnce = <T extends (...a: any[]) => void>(fn: T) => {
   // 当前版本 trick 用闭包保持fn的值
-  const fnRef  = useRef(fn)
-  fnRef.current = fn
+  const fnRef = useRef(fn);
+  fnRef.current = fn;
 
-  return useMemo(
-    () => {
-      let shouldRun = true
-      return (...args: Parameters<T>): void => {
-        if (shouldRun) {
-          shouldRun = false
-          fnRef.current(...args)
-        }
+  return useMemo(() => {
+    let shouldRun = true;
+    return (...args: Parameters<T>): void => {
+      if (shouldRun) {
+        shouldRun = false;
+        fnRef.current(...args);
       }
-    },
-    []
-  )
-}
+    };
+  }, []);
+};
 
 /**
  * 获得已经定义好的 space 的状态
@@ -49,18 +46,19 @@ const useSpaces = () => {
   const [spaces, setSpaces] = useState<Array<SpaceVo>>([]);
 
   const getLastedSpaces = () => {
-    return load().then(setSpaces)
-  }
+    return load().then(setSpaces);
+  };
 
-  const runWhenMount = useRunOnce(() => getLastedSpaces())
+  const runWhenMount = useRunOnce(() => getLastedSpaces());
 
-  useEffect(() => runWhenMount(), /* run once */ [])
+  useEffect(() => runWhenMount(), /* run once */ []);
 
   const [editingId, setEditingId] = useState<string>();
 
-  const modifyInLocal = (id: string) => (fn: (prevData: SpaceVo) => SpaceVo) => {
-    setSpaces((it) => it.map(it => it.id === id ? fn(it) : it))
-  }
+  const modifyInLocal =
+    (id: string) => (fn: (prevData: SpaceVo) => SpaceVo) => {
+      setSpaces((it) => it.map((it) => (it.id === id ? fn(it) : it)));
+    };
 
   const updateToServer = (data: SpaceVo) => {
     return fetch(`api/space/${data.id}`, {
@@ -72,12 +70,18 @@ const useSpaces = () => {
     })
       .then(filterMapWhenResponseOk)
       .then(getLastedSpaces)
-      .then(constTrue, constFalse)
+      .then(constTrue, constFalse);
+  };
 
-  }
-
-  return { spaces, getLastedSpaces, modifyInLocal, updateToServer, editingId, setEditingId } 
-}
+  return {
+    spaces,
+    getLastedSpaces,
+    modifyInLocal,
+    updateToServer,
+    editingId,
+    setEditingId,
+  };
+};
 
 /**
  * 创建 Space 所包含信息类型
@@ -88,10 +92,13 @@ type Pack = Pick<SpaceVo, "name" | "physicsPath">;
  * 创建新 space
  */
 const useCreatingSpace = () => {
-  const empty: Pack = { name: "", physicsPath: "" }
-  const [pack, setPack] = useState<Pack | undefined>();
+  const empty: Pack = { name: "", physicsPath: "" };
+  const [pack, setPack] = useState<Pack>({
+    name: "",
+    physicsPath: "",
+  });
 
-  const startCreating = () => setPack(empty)
+  const startCreating = () => setPack(empty);
 
   const postToServer = () => {
     return fetch(`api/space`, {
@@ -102,31 +109,45 @@ const useCreatingSpace = () => {
       },
     })
       .then(filterMapWhenResponseOk)
-      .then(() => setPack(undefined))
-      .then(constTrue, constFalse)
-  }
+      .then(() => setPack({ name: "", physicsPath: "" }))
+      .then(constTrue, constFalse);
+  };
 
-  return { startCreating, pack, setPack, postToServer }
-}
+  return { startCreating, pack, setPack, postToServer };
+};
 
 function App() {
-  const { spaces, getLastedSpaces, modifyInLocal, updateToServer, editingId, setEditingId } = useSpaces();
+  const {
+    spaces,
+    getLastedSpaces,
+    modifyInLocal,
+    updateToServer,
+    editingId,
+    setEditingId,
+  } = useSpaces();
 
-  const { startCreating, pack, setPack, postToServer } = useCreatingSpace()
+  const { startCreating, pack, setPack, postToServer } = useCreatingSpace();
 
   const doUpdate = (e: SpaceVo) => {
-      updateToServer(e)
-        .then((ok) => (ok ? noop : Toast.error.bind(Toast))("更新失败"))
-  }
+    updateToServer(e).then((ok) =>
+      (ok ? noop : Toast.error.bind(Toast))("更新失败")
+    );
+  };
 
   const doCreate = () => {
-    postToServer()
-      .then((ok) => (ok ? getLastedSpaces : (msg: string) => { Toast.error(msg) })("创建失败"))
+    postToServer().then((ok) =>
+      (ok
+        ? getLastedSpaces
+        : (msg: string) => {
+            Toast.error(msg);
+          })("创建失败")
+    );
   };
 
-  const modifyPhysicsPathOfExistSpace = (id: string) => (eventValue: string) => {
-    modifyInLocal(id)((space) => ({ ...space, physicsPath: eventValue }))
-  };
+  const modifyPhysicsPathOfExistSpace =
+    (id: string) => (eventValue: string) => {
+      modifyInLocal(id)((space) => ({ ...space, physicsPath: eventValue }));
+    };
 
   return (
     <div className="App" style={{ width: 1280 }}>
@@ -174,43 +195,43 @@ function App() {
       ) : (
         <div>
           <Empty title={"没有空间数据"} description="暂时没有任何的空间" />
-          <div style={{ margin: "8px", padding: "6px 24px" }}>
-            {pack && (
-              <div>
-                <Input
-                  placeholder="起个名字吧"
-                  value={pack.name}
-                  onChange={(value) => setPack({ ...pack, name: value })}
-                ></Input>
-                <Input
-                  style={{ marginTop: "8px" }}
-                  placeholder="输入物理路径"
-                  value={pack.physicsPath}
-                  onChange={(value) => setPack({ ...pack, physicsPath: value })}
-                ></Input>
-                <Button
-                  style={{ margin: "8px", padding: "6px 24px" }}
-                  theme="solid"
-                  type="primary"
-                  onClick={doCreate}
-                >
-                  保存
-                </Button>
-              </div>
-            )}
-            {!pack && (
-              <Button
-                disabled={!!pack}
-                style={{ margin: "8px", padding: "6px 24px" }}
-                theme="solid"
-                type="primary"
-                onClick={startCreating}
-              >
-                新建一个
-              </Button>
-            )}
-          </div>
         </div>
+      )}
+      <div style={{ margin: "8px", padding: "6px 24px" }}>
+        {pack && (
+          <div>
+            <Input
+              placeholder="起个名字吧"
+              value={pack.name}
+              onChange={(value) => setPack({ ...pack, name: value })}
+            ></Input>
+            <Input
+              style={{ marginTop: "8px" }}
+              placeholder="输入物理路径"
+              value={pack.physicsPath}
+              onChange={(value) => setPack({ ...pack, physicsPath: value })}
+            ></Input>
+            <Button
+              style={{ margin: "8px", padding: "6px 24px" }}
+              theme="solid"
+              type="primary"
+              onClick={doCreate}
+            >
+              保存
+            </Button>
+          </div>
+        )}
+      </div>
+      {!pack && (
+        <Button
+          disabled={!!pack}
+          style={{ margin: "8px", padding: "6px 24px" }}
+          theme="solid"
+          type="primary"
+          onClick={startCreating}
+        >
+          新建一个
+        </Button>
       )}
     </div>
   );
