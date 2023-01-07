354b999ad3a9077ef8bb5f04fed31561332c41b8
zhuangguangtuan <zhuangguangtuan@gmail.com>
2023-01-07 16:58:37 +0800

    tmp

---
+++ src/spaceDetail/ReFile.ts
@@ -0,0 +1,34 @@
+interface OsFile {
+    name: string,
+    fullpath: string
+}
+
+export class ReFile {
+    tag: string = "file"
+
+    osFile: OsFile
+    
+    parent: ReFolder
+
+    constructor(
+        osFile : OsFile,
+        parent: ReFolder,
+    ) {
+        this.osFile = osFile
+        this.parent = parent
+    }
+}
+
+export class ReFolder {
+    tag: string = "folder"
+
+    osFile: OsFile
+
+    constructor(osFile: OsFile) {
+        this.osFile = osFile
+    }
+}
+
+export OsFile
+
+export type ReResource = ReFile | ReFolder 
--- src/spaceDetail/spaceDetail.tsx
+++ src/spaceDetail/spaceDetail.tsx
@@ -1,6 +1,30 @@
 import { useEffect, useMemo, useRef, useState } from "react";
 import { useParams } from "react-router-dom";
+import { ReFolder, ReFile, OsFile } from './ReFile.ts'
 
+const parent = new ReFolder(
+    {
+        name: 'test',
+        fullpath: "/mnt/inner/test"
+    }
+)
+
+const mockData = [
+    new ReFile(
+        {
+            name: 'my heart will go on.mp3',
+            fullpath: "/mnt/inner/test/my heart will go on.mp3"
+        },
+        parent: parent
+    ),
+    new ReFile(
+        {
+            name: 'aaa.mp3',
+            fullpath: "/mnt/inner/test/aaa.mp3"
+        },
+        parent: parent
+    )
+]
 
 const SpaceDetail = () => {
     let { spaceId } = useParams<"spaceId">();
