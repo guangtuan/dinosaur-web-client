3fd965a9087cede1d158d939840cf4e6a4125bc0
guangtuan <zhuangguangtuan@gmail.com>
2023-01-15 21:42:34 +0800

    feat: wrapper of fetch(http request)

---
+++ src/lang/api.ts
@@ -0,0 +1,50 @@
+import { Notification } from "@douyinfe/semi-ui";
+import { GetContext, PostContext, Url } from "./apiType";
+
+const stringify = (obj: object): String => {
+  return Object.entries(obj)
+    .map(([k, v]) => `${k}=${encodeURI(v)}`)
+    .join("&");
+};
+
+export const urlize = (url: Url): string => {
+  return `${url.base}?${stringify(url.params)}`;
+};
+
+const filterMapWhenResponseOk = <T extends any>(res: Response): T | null => {
+  if (res.ok) {
+    return res.json() as T;
+  } else {
+    Notification.open({
+      content: "接口响应失败",
+    });
+    return null;
+  }
+};
+
+export const post = <T extends any>(
+  postContext: PostContext
+): Promise<T | null> => {
+  return fetch(urlize(postContext.url), {
+    method: "post",
+    body: JSON.stringify(postContext.body),
+    headers: {
+      "content-type": "application/json",
+    },
+  }).then((resp) => {
+    return filterMapWhenResponseOk(resp);
+  });
+};
+
+export const get = <T extends any>(
+  getContext: GetContext
+): Promise<T | null> => {
+  return fetch(urlize(getContext.url), {
+    method: "get",
+    headers: {
+      "content-type": "application/json",
+    },
+  }).then((resp) => {
+    return filterMapWhenResponseOk(resp);
+  });
+};
---
+++ src/lang/apiType.ts
@@ -0,0 +1,13 @@
+export type Url = {
+  base: string;
+  params: object;
+};
+
+export type PostContext = {
+  url: Url;
+  body: any;
+};
+
+export type GetContext = {
+  url: Url;
+};
