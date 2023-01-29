420f21bbf182baa0fd1f3a29d9f8ce3c7beb4bf9
guangtuan <zhuangguangtuan@gmail.com>
2023-01-29 22:37:58 +0800

    feat: display resource in table

--- src/spaceDetail/spaceDetail.tsx
+++ src/spaceDetail/spaceDetail.tsx
@@ -1,17 +1,32 @@
 import { useEffect, useMemo, useRef, useState } from "react";
 import { useParams } from "react-router-dom";
-import { ReFile, OsFile, ReResource, ReFolder } from "./ReFile";
-import { List } from "@douyinfe/semi-ui";
-import { IconMusic, IconImage, IconAlarm } from "@douyinfe/semi-icons";
+import { ReFile, ReResource, ReFolder, OsFile } from "./ReFile";
+import {
+  RadioGroup,
+  Radio,
+  Space,
+  Button,
+  Table
+} from "@douyinfe/semi-ui";
+import {
+  IconMusic,
+  IconImage,
+  IconAlarm,
+  IconFolder,
+  IconPlayCircle
+} from "@douyinfe/semi-icons";
 import { useSearchParams } from "react-router-dom";
 import { get } from "../lang/api";
 import useRunOnce from "../lang/useRunOnce";
 
 const API_RESOURCE_PREFIX = "/api/resources/";
 
-const getIcon = (osFile: OsFile) => {
-  const [_, suffix] = osFile.name.split(".");
+const getFileIcon = (suffix: string) => {
   switch (suffix) {
+    case "ts":
+    case "mp4":
+    case "rmvb":
+      return <IconPlayCircle style={{ color: "#673AB7" }} />
     case "mp3":
       return <IconMusic style={{ color: "#6A3AC7" }} />;
     case "jpg":
@@ -44,7 +59,7 @@ const useResource = () => {
 
   const runWhenMount = useRunOnce(() => loadResources(on));
 
-  useEffect(() => runWhenMount(), /* run once */ []);
+  useEffect(() => runWhenMount(), /* run once */[]);
 
   return {
     on,
@@ -54,14 +69,91 @@ const useResource = () => {
   };
 };
 
-const renderItem = (item: ReFile | ReFolder) => {
-  return (
-    <List.Item>
-      {getIcon(item.osFile)}
-      <span style={{ paddingLeft: "8px" }}>{item.osFile.name}</span>
-    </List.Item>
-  );
-};
+type RenderAbleItem = {
+  type: 'file' | 'folder',
+  name: string,
+  suffix: string,
+}
+
+const convert = (
+  f: ReFile | ReFolder,
+  index: number,
+  arr: Array<ReFile | ReFolder>
+): RenderAbleItem => {
+  const [_, suffix] = f.osFile.name.split(".");
+
+  return {
+    type: f.tag,
+    name: f.osFile.name,
+    suffix: suffix || ''
+  }
+}
+
+const columns = [
+  {
+    title: '文件',
+    render: (text: string, record: RenderAbleItem, index: number) => {
+      if (record.type === 'folder') {
+        return (
+          <Space>
+            <IconFolder style={{ color: "#CDDC39" }} />
+            <span style={{ paddingLeft: "8px" }}>{record.name}</span>
+          </Space>
+        )
+      } else {
+        return (
+          <Space>
+            {getFileIcon(record.suffix)}
+            <span style={{ paddingLeft: "8px" }}>{record.name}</span>
+          </Space>
+        )
+      }
+    },
+  },
+  {
+    title: '标记为',
+    render: (text: string, record: RenderAbleItem, index: number) => {
+      if (record.type === 'folder') {
+        return (
+          <Space>
+
+          </Space>
+        )
+      } else {
+        return (
+          <RadioGroup
+            type='button'
+            buttonSize='small'
+            defaultValue={1}
+          >
+            <Radio value={1}>剧集</Radio>
+            <Radio value={2}>电影</Radio>
+            <Radio value={3}>图片</Radio>
+            <Radio value={4}>音乐</Radio>
+          </RadioGroup>
+        )
+      }
+    },
+  },
+  {
+    title: '操作',
+    render: (text: string, record: RenderAbleItem, index: number) => {
+      if (record.type === 'folder') {
+        return (
+          <Space>
+
+          </Space>
+        )
+      } else {
+        return (
+          <Space>
+            <Button type="secondary">播放</Button>
+          </Space>
+        )
+      }
+    },
+  }
+]
 
 /**
  * url: space/63c249e21a2c97e086b4bdda?on=C:%5CUsers%5Cgrant%5CPictures
@@ -76,10 +168,10 @@ const SpaceDetail = () => {
 
   return (
     <div>
-      <List
-        bordered
-        dataSource={resource?.children || []}
-        renderItem={renderItem}
+      <Table
+        pagination={false}
+        dataSource={(resource?.children || []).map(convert)}
+        columns={columns}
       />
     </div>
   );
