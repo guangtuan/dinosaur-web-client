370e3963cf83ee02b08f067419ae1dfbd60aa6d4
zhuangguangtuan <zhuangguangtuan@gmail.com>
2023-03-05 17:09:02 +0800

    chore: 可配置的开发服务器地址

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+const devServer: string = process.env["DINOSAUR_DEV_SERVER"]!!;
+
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:15889",
+        // target: "http://192.168.31.197:15889",
+        target: devServer,
         changeOrigin: true,
         rewrite: (path) => {
           console.log("rewrite ", path);
