5d6517df73437e324b2d87698c4f237f355503fe
zhuangguangtuan <zhuangguangtuan@gmail.com>
2023-03-12 15:51:19 +0800

    refactor: extract function

--- src/lang/api.ts
+++ src/lang/api.ts
@@ -11,8 +11,28 @@ export const urlize = (url: Url): string => {
   return `${url.base}?${stringify(url.params || {})}`;
 };
 
-const filterMapWhenResponseOk = <T extends any>(res: Response): T | null => {
+function wrapErrorHandler<T>(fn: () => Promise<T | null>) {
+  try {
+    return fn();
+  } catch (e) {
+    console.error(e);
+    Notification.open({
+      content: `接口响应失败 ${e}`,
+    });
+    return null;
+  }
+}
+
+const filterMapWhenResponseOk = <T extends any>(
+  res: Response,
+  successMessage: string | null = null
+): T | null => {
   if (res.ok) {
+    if (successMessage !== null) {
+      Notification.open({
+        content: successMessage,
+      });
+    }
     return res.json() as T;
   } else {
     Notification.open({
@@ -25,7 +45,7 @@ const filterMapWhenResponseOk = <T extends any>(res: Response): T | null => {
 export const post = async <T extends any>(
   postContext: PostContext
 ): Promise<T | null> => {
-  try {
+  return wrapErrorHandler(async () => {
     const resp = await fetch(urlize(postContext.url), {
       method: "post",
       body: JSON.stringify(postContext.body),
@@ -33,20 +53,14 @@ export const post = async <T extends any>(
         "content-type": "application/json",
       },
     });
-    return filterMapWhenResponseOk(resp);
-  } catch (e) {
-    console.error(e);
-    Notification.open({
-      content: `接口响应失败 ${e}`,
-    });
-    return null;
-  }
+    return filterMapWhenResponseOk(resp, "创建成功");
+  });
 };
 
 export const put = async <T extends any>(
   putContext: PutContext
 ): Promise<T | null> => {
-  try {
+  return wrapErrorHandler(async () => {
     const resp = await fetch(urlize(putContext.url), {
       method: "put",
       body: JSON.stringify(putContext.body),
@@ -54,20 +68,14 @@ export const put = async <T extends any>(
         "content-type": "application/json",
       },
     });
-    return filterMapWhenResponseOk(resp);
-  } catch (e) {
-    console.error(e);
-    Notification.open({
-      content: `接口响应失败 ${e}`,
-    });
-    return null;
-  }
+    return filterMapWhenResponseOk(resp, "更新成功");
+  });
 };
 
 export const get = async <T extends any>(
   getContext: GetContext
 ): Promise<T | null> => {
-  try {
+  return wrapErrorHandler(async () => {
     const resp = await fetch(urlize(getContext.url), {
       method: "get",
       headers: {
@@ -75,11 +83,5 @@ export const get = async <T extends any>(
       },
     });
     return filterMapWhenResponseOk(resp);
-  } catch (e) {
-    console.error(e);
-    Notification.open({
-      content: `接口响应失败 ${e}`,
-    });
-    return null;
-  }
+  });
 };
