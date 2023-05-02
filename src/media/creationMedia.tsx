f4e119659c4e971b3a749b8a17eaae013ca40ec6
zhuangguangtuan <zhuangguangtuan@gmail.com>
2023-05-02 19:55:06 +0800

    feat: pop dialog create mdeia

--- src/media/creationMedia.tsx
+++ src/media/creationMedia.tsx
@@ -1,14 +1,58 @@
-import { Modal, Form, Input } from "@douyinfe/semi-ui";
-import { useEffect, useState } from "react";
+import { Modal, Form, Input, Button, Divider } from "@douyinfe/semi-ui";
+import { Pack } from "./model";
+import { assoc } from "ramda";
+import { Dispatch } from "react";
+import { Rating } from "@douyinfe/semi-ui";
+import { IconStar } from "@douyinfe/semi-icons";
 
-const CreationMedia = () => {
-    return <Modal
-        visible={true}
+type MyProp = {
+  pack: Pack;
+  setPack: Dispatch<Pack>;
+};
+
+const CreationMedia = (p: MyProp) => {
+  const { pack, setPack } = p;
+  const st = { fontSize: 24 };
+  return (
+    <Modal
+      onOk={() => {
+        console.log("save");
+        // TODO: call and create
+        setPack(assoc("c", false, pack));
+      }}
+      onCancel={() => {
+        console.log("cancel");
+        setPack(assoc("c", false, pack));
+      }}
+      visible={true}
     >
-        <Form>
-            <Input placeholder="name"></Input>
-        </Form>
+      <Form>
+        <Divider />
+        <Input
+          style={st}
+          placeholder="name"
+          value={pack.name}
+          onChange={(v) => setPack(assoc("name", v, pack))}
+        ></Input>
+        <Divider />
+        <Input
+          style={assoc("marginTop", "8px", st)}
+          placeholder="cover"
+          value={pack.cover}
+          onChange={(v) => setPack(assoc("cover", v, pack))}
+        ></Input>
+        <Divider />
+        <Rating
+          value={pack.rating || 0}
+          style={{ color: "rgba(var(--semi-blue-5), 1)", marginTop: "8px" }}
+          size={48}
+          allowHalf
+          character={<IconStar style={st} />}
+          onChange={(v) => setPack(assoc("rating", v, pack))}
+        />
+      </Form>
     </Modal>
-}
+  );
+};
 
 export default CreationMedia;
